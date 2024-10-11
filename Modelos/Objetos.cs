69c814e80883a49feb068300059d46d970af42e4
Cristopher <cristophersp706@gmail.com>
2024-10-11 19:31:16 -0400

    v1

---
+++ Modelos/Objetos.cs
@@ -0,0 +1,33 @@
+public enum TipoEntidad{
+    Serie = 'S',
+    Pelicula = 'P',
+    Libro = 'L'
+}
+
+public class SeriePeliculaLibro{
+    public int Id {get; set;}
+    public string Nombre {get; set;} = "";
+    public TipoEntidad Tipo {get; set;} = TipoEntidad.Serie;
+    public string Pais {get; set;} = "";
+    public string Idioma {get; set;} = "";
+    public string FotoUrl {get; set;} = "";
+    public string Resumen {get; set;} = "";
+
+//Relacion con los personajes
+public List<Personaje> Personajes {get; set; } = null!;
+
+}
+
+public class Personaje{
+    public int Id {get; set;}
+    public string Nombre {get; set;} = "";
+    public string Apodo {get; set;} = "";
+    public string Raza {get; set;} = "";
+    public string FotoUrl {get; set;} = "";
+    public int Edad {get; set;} = 0;
+    public string PoderCaracteristico {get; set;} = "";
+
+    // Relacionado con SeriePeliculaLibro
+    public int SeriePeliculaLibroId {get; set; }
+    public SeriePeliculaLibro seriePeliculaLibro {get; set; } = null!;
+}
